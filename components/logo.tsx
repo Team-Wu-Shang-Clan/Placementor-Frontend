b3d845c654361b47621905bdc814577a63eca441
anurag-kochar <anuragmarketing101@gmail.com>
2025-02-27 15:10:21 +0530

    add: logo component

---
+++ components/logo.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+type Props = {
+    href?: string;
+    isLogoOnly?: boolean;
+    debugData?: unknown
+    classes?: {
+        text?: string
+    }
+};
+
+export function Logo({ href = "/", isLogoOnly, classes }: Props) {
+    return (
+        <Link href={href} className={cn("flex-shrink-0 flex gap-2 items-center")}>
+            {!isLogoOnly && (
+                <span className={cn("text-sm italic font-bold text-foreground", classes?.text)}>
+                    Place<span className="bg-primary underline">mentor</span> AI
+                </span>
+            )}
+        </Link>
+    );
+}
