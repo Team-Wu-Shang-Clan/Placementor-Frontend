ffeb51d5682031ab833093a2f9b2ebc205ccd7f9
Tamanna Sharma <ts.harma2882006@gmail.com>
2025-02-27 20:26:04 +0530

    checkpnt

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,19 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import {HeroSection} from './_components/hero'
+import {BenefitsSection} from './_components/benefits';
+import {FeaturesSection} from './_components/features';
+import {ServicesSection} from './_components/services';
+import {TestimonialSection} from './_components/testimonials';
+import {TeamSection} from './_components/team';
 
 const Page = () => {
   return (
-    <div className='flex h-screen w-full justify-center items-center'>
-      <Button> Hello </Button>
+    <div className='flex flex-col min-h-screen w-full justify-center items-center'>
+      <HeroSection />
+      <BenefitsSection />
+      <FeaturesSection />
+      <ServicesSection />
+      <TestimonialSection />
+      <TeamSection />
     </div>
   )
 }
